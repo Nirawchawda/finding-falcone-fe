53457d857d17b419d50c85923bec277602a952ba
Nirawchawda <it13015.niraw.chawda@gmail.com>
2020-07-04 02:06:12 +0530

    Final Deploy corrections changing name to finding-falcone

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/FindingFalcone'));
+app.use(express.static(__dirname + '/dist/finding-falcone'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/FindingFalcone/index.html'));
+res.sendFile(path.join(__dirname+'/dist/finding-falcone/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
