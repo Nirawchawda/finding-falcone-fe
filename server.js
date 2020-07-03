d7fd1b87d19538de8f340d047d0bc7f615016bbc
Nirawchawda <it13015.niraw.chawda@gmail.com>
2020-07-04 01:54:04 +0530

    changing server.js app name

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/finding-falcone-fe'));
+app.use(express.static(__dirname + '/dist/finding-falcone'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/finding-falcone-fe/index.html'));
+res.sendFile(path.join(__dirname+'/dist/finding-falcone/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
